dd502ae1b6f3a5ec6591dc6b6af42aff89f1e707
Sadvika7reddy <sadvika7reddy@gmail.com>
2023-11-05 18:49:14 +0530

    customized Radixui themes

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,11 @@ import '@radix-ui/themes/styles.css';
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import Navbar from './Navbar'
-import { Theme } from '@radix-ui/themes';
-const inter = Inter({ subsets: ['latin'] })
+import { Theme, ThemePanel } from '@radix-ui/themes';
+const inter = Inter({ 
+  subsets: ['latin'],
+  variable:'--font-inter'
+ })
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -18,11 +21,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={inter.variable}>
         <Theme>
         <Navbar/>
         <main className='p-5'>{children}</main>
         </Theme>
+
         </body>
     </html>
   )
