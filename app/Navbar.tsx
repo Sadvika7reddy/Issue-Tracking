1a8dd5b54bf916f71a0864f1ea4a48976f5a6e54
Sadvika7reddy <sadvika7reddy@gmail.com>
2023-11-05 14:53:35 +0530

    active navbar

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,12 +1,23 @@
+'use client';
 import React from 'react'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 const Navbar = () => {
+    const currentPath=usePathname();
+    const links=[
+        {label:'Dashboard',href:'/'},
+        {label:'Issues',href:'/issues'}
+    ]
   return (
     <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center'>
         <Link href='/'>Logo</Link>
         <ul className='flex space-x-6'>
-            <li><Link className='text-zinc-500 hover:text-zinc-800' href='/'>Dashboard</Link></li>
-            <li><Link  className='text-zinc-500 hover:text-zinc-800' href='/issues'>Issues</Link></li>
+            {links.map(link=>
+                  <Link key='link.href' className={`${link.href===currentPath?'text-zinc-900':'text-zinc-500'} hover:text-zinc-800`}
+                   href={link.href}>{link.label}</Link>
+            )}
+            
+            
         </ul>
     </nav>
   )
